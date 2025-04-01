7264295af6df18c58c581931f6d61fe696e82534
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2025-04-01 18:24:07 +1100

    Initial Commit

---
+++ ios/Freerooms/FreeroomsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreeroomsApp.swift
+//  Freerooms
+//
+//  Created by Anh Nguyen on 1/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct FreeroomsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
