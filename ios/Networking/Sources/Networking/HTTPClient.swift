185bc7a48a0ef8d9ea7b90f48e5cbddbe7c492dc
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2025-04-07 16:06:11 +1000

    feat: add Networking module with HTTPClient protocol

---
+++ ios/Networking/Sources/Networking/HTTPClient.swift
@@ -0,0 +1,22 @@
+//
+//  HTTPClient.swift
+//  Networking
+//
+//  Created by Anh Nguyen on 31/1/2025.
+//
+
+import Foundation
+
+// MARK: - HTTPClient
+
+public protocol HTTPClient {
+  typealias Result = Swift.Result<(Data, HTTPURLResponse), Error>
+
+  func get(from url: URL) async -> Result
+}
+
+// MARK: - HTTPClientError
+
+public enum HTTPClientError: Error {
+  case networkFailure
+}
