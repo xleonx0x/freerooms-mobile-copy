71fddea1a33086621e550adb24f01e1422e0981e
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2025-04-23 00:47:30 +1000

    feat: add URLSessionHTTPClient

---
+++ ios/Networking/Sources/Networking/URLSessionHTTPClient.swift
@@ -0,0 +1,14 @@
+//
+//  URLSessionHTTPClient.swift
+//  Networking
+//
+//  Created by Anh Nguyen on 23/4/2025.
+//
+
+import Foundation
+
+struct URLSessionHTTPClient: HTTPClient {
+  func get(from url: URL) async -> HTTPClient.Result {
+    fatalError("TODO: Implement")
+  }
+}
