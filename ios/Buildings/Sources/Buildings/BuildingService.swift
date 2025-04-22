55550f0d5ddbbfea551b7c5591842977dc8c1b24
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2025-04-23 00:45:18 +1000

    feat: add BuildingService

---
+++ ios/Buildings/Sources/Buildings/BuildingService.swift
@@ -0,0 +1,14 @@
+//
+//  BuildingService.swift
+//  Buildings
+//
+//  Created by Anh Nguyen on 22/4/2025.
+//
+
+import Foundation
+
+class BuildingService {
+  func getBuildings() -> [Building] {
+    fatalError("TODO: Implement")
+  }
+}
