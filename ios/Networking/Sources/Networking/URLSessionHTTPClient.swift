97065d8de42c8c23ce70da048c48cab1cdf97305
Muqueet Mohsen Chowdhury <mmc@berkeley.edu>
2025-04-26 02:37:18 +1000

    Implemented MapView Preview with sample buildings

--- ios/Networking/Sources/Networking/URLSessionHTTPClient.swift
+++ ios/Networking/Sources/Networking/URLSessionHTTPClient.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct URLSessionHTTPClient: HTTPClient {
-  func get(from url: URL) async -> HTTPClient.Result {
+  func get(from _: URL) async -> HTTPClient.Result {
     fatalError("TODO: Implement")
   }
 }
