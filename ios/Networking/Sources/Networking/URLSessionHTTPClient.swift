0f16af99e2b7b39505d3f402c2f1190ef119af5a
Elvene <123799877+xleonx0x@users.noreply.github.com>
2025-04-26 18:23:34 +1000

    feat: implemented get method in URLSessionHTTPClient

--- ios/Networking/Sources/Networking/URLSessionHTTPClient.swift
+++ ios/Networking/Sources/Networking/URLSessionHTTPClient.swift
@@ -7,8 +7,27 @@
 
 import Foundation
 
-struct URLSessionHTTPClient: HTTPClient {
-  func get(from url: URL) async -> HTTPClient.Result {
-    fatalError("TODO: Implement")
+// MARK: - URLSessionHTTPClient
+
+@available(macOS 12.0, *)
+public struct URLSessionHTTPClient: HTTPClient {
+  private var urlSession: URLSessionProtocol
+
+  public init(urlSession: URLSessionProtocol) {
+    self.urlSession = urlSession
+  }
+
+  public func get(from url: URL) async -> HTTPClient.Result {
+    do {
+      let (data, urlResponse) = try await urlSession.data(from: url)
+      guard let httpUrlResponse = urlResponse as? HTTPURLResponse else {
+        return .failure(HTTPClientError.invalidHTTPResponse)
+      }
+      return .success((data, httpUrlResponse))
+    } catch let networkError as URLError where networkError.code == .networkConnectionLost || networkError.code == .notConnectedToInternet {
+      return .failure(HTTPClientError.networkFailure)
+    } catch let error {
+      return .failure(error)
+    }
   }
 }
