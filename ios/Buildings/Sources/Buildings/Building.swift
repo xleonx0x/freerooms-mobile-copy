8a1652a4a7fcfd1dc7a775ead21c3dfa59b698b2
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2025-04-27 22:35:28 +1000

    feat: add number of available rooms to Building struct

--- ios/Buildings/Sources/Buildings/Building.swift
+++ ios/Buildings/Sources/Buildings/Building.swift
@@ -11,12 +11,14 @@ public struct Building: Equatable {
   public let latitude: Double
   public let longitude: Double
   public let aliases: [String]
+  public let numberOfAvailableRooms: Int?
 
-  public init(name: String, id: String, latitude: Double, longitude: Double, aliases: [String]) {
+  public init(name: String, id: String, latitude: Double, longitude: Double, aliases: [String], numberOfAvailableRooms: Int?) {
     self.name = name
     self.id = id
     self.latitude = latitude
     self.longitude = longitude
     self.aliases = aliases
+    self.numberOfAvailableRooms = numberOfAvailableRooms
   }
 }
