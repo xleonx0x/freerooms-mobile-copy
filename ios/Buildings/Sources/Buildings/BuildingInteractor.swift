ad06b9de6f34839acfdb74cd2865ceb49a6caff3
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2025-04-27 22:34:33 +1000

    feat: add BuildingInteractor

---
+++ ios/Buildings/Sources/Buildings/BuildingInteractor.swift
@@ -0,0 +1,22 @@
+//
+//  BuildingInteractor.swift
+//  Buildings
+//
+//  Created by Anh Nguyen on 27/4/2025.
+//
+
+import Foundation
+
+final class BuildingInteractor {
+  func getBuildingsSortedByAvailableRooms() -> [Building] {
+    fatalError("TODO: Implement")
+  }
+  
+  func getBuildingsSortedAlphabetically(inAscendingOrder isAscending: Bool) -> [Building] {
+    fatalError("TODO: Implement")
+  }
+  
+  func getBuildingsSortedByDistance(inAscendingOrder isAscending: Bool) -> [Building] {
+    fatalError("TODO: Implement")
+  }
+}
